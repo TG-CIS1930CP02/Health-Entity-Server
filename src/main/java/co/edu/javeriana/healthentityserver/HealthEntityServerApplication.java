2fc987731591386b0197e2c47ffbd08b1293bf8c
Laura Donado <ldonado11@gmail.com>
2020-02-23 18:39:37 -0500

    Initial structure for server

---
+++ src/main/java/co/edu/javeriana/healthentityserver/HealthEntityServerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.HealthEntityServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthEntityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthEntityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/healthentityserver/HealthEntityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.HealthEntityServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthEntityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
