e92b61d64811e891812e32f7d2118b9c1668e3b2
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-10 18:48:03 -0500

    Service for get patient example

--- src/main/java/co/edu/javeriana/healthentityserver/restservices/PatientService.java
+++ src/main/java/co/edu/javeriana/healthentityserver/restservices/PatientService.java
@@ -5,12 +5,15 @@ import java.util.UUID;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 import co.edu.javeriana.healthentityserver.mongodb.MongoDBClient;
 import co.edu.javeriana.healthentityserver.security.ServerIdentification;
@@ -30,5 +33,15 @@ public class PatientService {
 		String uuid = UUID.randomUUID().toString();	
 		patientDocument.replace("id", uuid);
 		collection.insertOne(patientDocument);
+	}
+	
+	@GetMapping("/patient/{resourceId}")
+	@PreAuthorize("hasRole('ROLE_DOCTOR') and hasAuthority('PASSWORD_AND_FINGERPRINT_AUTHENTICATED_USER') and "
+			+ "hasAuthority(@serverIdentification.getRoleHealthEntity())")
+	public Object getPatient(@PathVariable String resourceId, @RequestHeader("Authorization") String token) {
+		MongoCollection<Document> collection = mongoDBClient.getPatientCollection();
+		Document patient = collection.find(Filters.eq("id", resourceId)).first();
+		patient.remove("_id");
+		return patient;
 	}
 }
