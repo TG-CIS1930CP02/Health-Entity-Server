f044f15260072f88101453668c6279c0c1eaa32e
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-28 11:58:16 -0500

    services for get url of server

--- src/main/java/co/edu/javeriana/healthentityserver/security/ServerIdentification.java
+++ src/main/java/co/edu/javeriana/healthentityserver/security/ServerIdentification.java
@@ -4,11 +4,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("serverIdentification")
-public class ServerIdentification {
+public class ServerIdentification {	
+	@Value("${server.protocol}")
+	private String serverProtocol;
+	
 	@Value("${role.health.entity}")
 	private String roleHealthEntity;
+	
+	@Value("${server.address}")
+	private String serverAddress;
+	
+	@Value("${server.port}")
+	private String serverPort;
 
 	public String getRoleHealthEntity() {
 		return roleHealthEntity;
+	}
+
+	public String getServerAddress() {
+		return serverAddress;
+	}
+
+	public String getServerPort() {
+		return serverPort;
+	}
+	
+	public String getServerProtocol() {
+		return serverProtocol;
+	}
+
+	public String getServerUrl() {
+		return serverProtocol + "://" + serverAddress + ":" + serverPort + "/";
 	}
 }
