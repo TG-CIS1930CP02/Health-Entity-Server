b689fab83637b9d2060bdeecda1889740a09794d
OmarEspinel <espinelo@javeriana.edu.co>
2020-04-29 10:30:06 -0500

    Conection to bc searches services added

---
+++ src/main/java/co/edu/javeriana/healthentityserver/blockchain/NetworkSearch.java
@@ -0,0 +1,101 @@
+package co.edu.javeriana.healthentityserver.blockchain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import co.edu.javeriana.healthentityserver.enums.OperationEnum;
+import co.edu.javeriana.healthentityserver.enums.ResourceTypeEnum;
+import co.edu.javeriana.healthentityserver.enums.RoleEnum;
+import co.edu.javeriana.healthentityserver.security.ServerIdentification;
+
+public class NetworkSearch {
+	
+	public static List<Transaction> sendSearch(String path, String blockchainUrl) {
+		
+		List<Transaction> transactions = new ArrayList<Transaction>();
+		
+		try {
+			//http://localhost:5000/search?search=Resources&recipient=bbb3
+			URL url = new URL(blockchainUrl+path);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json; utf-8");
+			con.setRequestProperty("Accept", "application/json");
+			con.setDoOutput(true);
+			
+			int responseCode = con.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				System.out.println("Response: ok ");
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+				
+				JSONArray jsonResponse = new JSONArray(response.toString());
+				
+				
+		        for (int i = 0; i < jsonResponse.length(); i++){
+		            JSONObject jsonObj = jsonResponse.getJSONObject(i);
+		            
+		            //System.out.println(jsonObj);
+		            
+		            //Create transaction
+		            Transaction mTransaction = new Transaction();
+		    		mTransaction.setInstitution(jsonObj.get("institution").toString());
+		    		mTransaction.setSenderRole(Enum.valueOf(RoleEnum.class, jsonObj.get("sender_role").toString()));
+		    		mTransaction.setSender(jsonObj.get("sender").toString());
+		    		mTransaction.setRecipientRole(Enum.valueOf(RoleEnum.class, jsonObj.get("recipient_role").toString()));
+		    		mTransaction.setRecipient(jsonObj.get("recipient").toString());	
+		    		mTransaction.setOperation(Enum.valueOf(OperationEnum.class, jsonObj.get("operation").toString()));
+		    		mTransaction.setTimestamp(new Timestamp(  Long.parseLong(jsonObj.get("timestamp").toString())  ));
+		    		mTransaction.setResourceIntegrity( jsonObj.get("institution").toString() );
+		    		mTransaction.setResourceType(Enum.valueOf(ResourceTypeEnum.class, jsonObj.get("resource_type").toString()));
+		    		mTransaction.setResourcePath(jsonObj.get("institution").toString());
+		    		
+		    		transactions.add(mTransaction);
+		    		
+		        }
+		        // print result
+				//System.out.println(response.toString());
+			
+			} else {
+				
+			}
+			return transactions;	
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return transactions;
+		
+	}
+	
+	
+}
--- src/main/java/co/edu/javeriana/healthentityserver/restservices/BlockchainService.java
+++ src/main/java/co/edu/javeriana/healthentityserver/restservices/BlockchainService.java
@@ -3,35 +3,51 @@ package co.edu.javeriana.healthentityserver.restservices;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.javeriana.healthentityserver.enums.IdentificationTypeEnum;
+import co.edu.javeriana.healthentityserver.security.ServerIdentification;
 import co.edu.javeriana.healthentityserver.blockchain.*;
 
 @RestController
 public class BlockchainService {
+	@Autowired
+	private ServerIdentification serverIdentification;
 	
 	@GetMapping("/transactions/patient/{identificationType}/{identificationNumber}")
 	public List<Transaction> getTransactions (@PathVariable IdentificationTypeEnum identificationType, 
 			@PathVariable Long identificationNumber ){
-		//AQUI BUSQUEDA DE TRANSACCIONES
-		return new ArrayList<>();		
+		
+		String path = String.format("search?search=Resources&recipient=%s", identificationNumber);
+
+		List<Transaction> response = NetworkSearch.sendSearch(path, serverIdentification.getBcserverUrl());
+		
+		return response;		
 	}
 	
 	@GetMapping("/transactions/medical-history/patient/{identificationType}/{identificationNumber}")
 	public List<Transaction> getMedicalRecords (@PathVariable IdentificationTypeEnum identificationType, 
 			@PathVariable Long identificationNumber ){
-		//AQUI BUSQUEDA DE TRANSACCIONES
-		return new ArrayList<>();		
+
+		String path = String.format("search?search=History&recipient=%s", identificationNumber);
+
+		List<Transaction> response = NetworkSearch.sendSearch(path, serverIdentification.getBcserverUrl());
+
+		return response;		
 	}
 	
 	@GetMapping("/transactions/emergency-medical-history/patient/{identificationType}/{identificationNumber}")
 	public List<Transaction> getEmergencyMedicalHistory (@PathVariable IdentificationTypeEnum identificationType, 
 			@PathVariable Long identificationNumber ){
-		//AQUI BUSQUEDA DE TRANSACCIONES
-		return new ArrayList<>();		
+
+		String path = String.format("search?search=Emergency&recipient=%s", identificationNumber);
+
+		List<Transaction> response = NetworkSearch.sendSearch(path, serverIdentification.getBcserverUrl());
+		
+		return response;		
 	}
 		
 }
