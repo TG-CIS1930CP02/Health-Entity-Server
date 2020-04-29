cdc7fec8c535472b2665a73e75c3a65cd8a70ec8
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-28 19:31:04 -0500

    Services for get information from JWTToken

--- src/main/java/co/edu/javeriana/healthentityserver/security/JWTTokenProcessor.java
+++ src/main/java/co/edu/javeriana/healthentityserver/security/JWTTokenProcessor.java
@@ -5,19 +5,49 @@ import java.util.Base64;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JWTTokenProcessor {
 	
-	public String getPayloadBase64(String token){
+	public List<String> getAuthorities(String token){
 		List<String> parts = new ArrayList<String>();
 		StringTokenizer stok = new StringTokenizer(token, ".");
 		while (stok.hasMoreTokens()){
 			parts.add(stok.nextToken());
 		}
-		return parts.get(1);
+		String payload = new String(Base64.getDecoder().decode(parts.get(1)));
+		JSONObject payloadObject = new JSONObject(payload);
+		JSONArray arr = payloadObject.getJSONArray("authorities");
+		List<String> list = new ArrayList<String>();
+		for(int i = 0; i < arr.length(); i++){
+		    list.add(arr.getJSONObject(i).toString());
+		}
+		return list;
 	}
+	
+	public String getAuthenticationMode(String token){
+		List<String> parts = new ArrayList<String>();
+		StringTokenizer stok = new StringTokenizer(token, ".");
+		while (stok.hasMoreTokens()){
+			parts.add(stok.nextToken());
+		}
+		String payload = new String(Base64.getDecoder().decode(parts.get(1)));
+		JSONObject payloadObject = new JSONObject(payload);
+		return payloadObject.get("authenticationMode").toString();	
+	}
+	
+	public String getLoginTime(String token){
+		List<String> parts = new ArrayList<String>();
+		StringTokenizer stok = new StringTokenizer(token, ".");
+		while (stok.hasMoreTokens()){
+			parts.add(stok.nextToken());
+		}
+		String payload = new String(Base64.getDecoder().decode(parts.get(1)));
+		JSONObject payloadObject = new JSONObject(payload);
+		return payloadObject.get("iat").toString();	
+	} 
 	
 }
